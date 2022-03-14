7b1d0b1a398ec16de2b6de663be270d90fada2a5
KevinDaGame <kevindolfing2@gmail.com>
2022-03-14 12:17:02 +0100

    Added address to form

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('title')
-    <title>Register</title>
+    <title>{{__('Register')}}</title>
 @endsection()
 @section('body')
     <main class="mt-5 text-center mx-auto w-75 bg-light border border-dark rounded-2">
@@ -21,7 +21,7 @@
             </div>
 
             <div class="mb-6">
-                <label for="middle-name">Middle name</label>
+                <label for="middle-name">{{__('Middle name')}}</label>
                 <input
                     type="text"
                     name="middle-name"
@@ -33,7 +33,7 @@
                 @enderror
             </div>
             <div class="mb-6">
-                <label for="last-name">Last name</label>
+                <label for="last-name">{{__('Last name')}}</label>
                 <input
                     type="text"
                     name="last-name"
@@ -41,6 +41,103 @@
                     required
                 >
                 @error('last-name')
+                <p class="text-red">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <label for="email">{{__('Email address')}}</label>
+                <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    required
+                >
+                @error('email')
+                <p class="text-red">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <label for="phone-number">{{__('Phone number')}}</label>
+                <input
+                    type="text"
+                    name="phone-number"
+                    id="phone-number"
+                    required
+                >
+                @error('phone-number')
+                <p class="text-red">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <label for="city">{{__('City')}}</label>
+                <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    required
+                >
+                @error('city')
+                <p class="text-red">{{$message}}</p>
+                @enderror
+                <div class="mb-6">
+                    <label for="country">{{__('Country')}}</label>
+                    <input
+                        type="text"
+                        name="country"
+                        id="country"
+                        required
+                    >
+                    @error('country')
+                    <p class="text-red">{{$message}}</p>
+                    @enderror
+                </div>
+
+            </div>
+            <div class="mb-6">
+                <label for="street">{{__('Street')}}</label>
+                <input
+                    type="text"
+                    name="street"
+                    id="street"
+                    required
+                >
+                @error('street')
+                <p class="text-red">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <label for="postal_code">{{__('Postal code')}}</label>
+                <input
+                    type="text"
+                    name="postal_code"
+                    id="postal_code"
+                    required
+                >
+                @error('postal_code')
+                <p class="text-red">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <label for="house_number">{{__('House number')}}</label>
+                <input
+                    type="text"
+                    name="house_number"
+                    id="house_number"
+                    required
+                >
+                @error('house_number')
+                <p class="text-red">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <label for="addition">{{__('Addition')}}</label>
+                <input
+                    type="text"
+                    name="addition"
+                    id="addition"
+                    required
+                >
+                @error('addition')
                 <p class="text-red">{{$message}}</p>
                 @enderror
             </div>
