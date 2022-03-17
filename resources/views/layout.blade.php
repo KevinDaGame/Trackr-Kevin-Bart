00f7754db55d6c9949e4030ed556dfd133c3fc1c
KevinDaGame <kevindolfing2@gmail.com>
2022-03-17 19:18:35 +0100

    Fixed bootstrap javascript, fixed success flash popup

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/bootstrap.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     @yield('title')
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -56,7 +57,7 @@
     @yield('body')
 </div>
 @if(session()->has('success'))
-    <div class="alert alert-dismissible alert-success">
+    <div class="alert alert-dismissible alert-success position-fixed bottom-0 end-0 m-3">
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         <p>{{__(session()->get('success'))}}</p>
     </div>
