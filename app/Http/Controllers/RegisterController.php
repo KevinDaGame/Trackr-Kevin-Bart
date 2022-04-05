cdea8c710c546eb31f607d18836a36d437ce8f9d
KevDaDev <65958288+KevinDaGame@users.noreply.github.com>
2022-04-05 12:07:34 +0200

    Feature/register test (#45)
    * Removed address from register
    
    * Create RegisterTest.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,28 +23,12 @@ class RegisterController extends Controller
             'last-name' => 'required|max:255|min:2',
             'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|max:255|min:8',
-            'country' => 'required|max:255',
-            'city' => 'required|max:255',
-            'street' => 'required|max:255',
-            'postal_code' => 'required|postal_code:NL,DE,FR,BE,LU',
-            'house_number' => 'required|numeric',
-            'addition' => 'max:2',
-        ]);
-
-        $address = Address::firstOrCreate([
-            'country' => request()->get('country'),
-            'street' => request()->get('street'),
-            'city' => request()->get('city'),
-            'postal_code' => request()->get('postal_code'),
-            'house_number' => request()->get('house_number'),
-            'addition' => request()->get('addition'),
         ]);
 
         $user = User::create([
             'name' => request()->get('first-name') . ' ' . request()->get('last-name'),
             'email' => request()->get('email'),
             'password' => bcrypt(request()->get('password')),
-            'address_id' => $address->id,
         ]);
         $user->attachRole(config('roles.models.role')::where('name', '=', 'Customer')->first());
         auth()->login($user);
